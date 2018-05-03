1606d43716214ec884f0158c6d7359417c66a4b9
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-05-03 01:22:32 +0000

    :wq  Please enter the commit message for your changes. Lines starting

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -6,14 +6,13 @@ namespace BookCave.Data
     public class DataContext : DbContext
     {
         public DbSet<Book> Books { get; set; }
-        public DbSet<Author>  Authors { get; set; }
+        public DbSet<Author> Authors { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder
                 .UseSqlServer(
-                    ""
-                );
+                    "Server=tcp:verklegt2.database.windows.net,1433;Initial Catalog=VLN2_2018_H26;Persist Security Info=False;User ID=VLN2_2018_H26_usr;Password=fu2zyPump73;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         }
     }
 }
