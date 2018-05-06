0510021ea0ce370ae88954388712a97cb8cf5faa
Thelma Rut <thelmaj16@ru.is>
2018-05-06 19:22:31 +0000

    bæting
    bókabæting

--- Program.cs
+++ Program.cs
@@ -32,14 +32,14 @@ namespace BookCave
 
                 var initialBooks = new List<Book>()
                 {
-                    new Book { Title = "Developing Fraction Knowledge", Description ="Supporting and understanding your students' fractional knowledge is crucial to their overall grasp of numbers and mathematics. By centralizing around three key stages of development, this effective guide will help you to assess your students' understanding of fractions and modify your teaching accordingly." , Price = 12, Rating = 7, ReleaseYear = 2016, Image = "", AuthorId = 19 },
+                    new Book { Title = "The Fault in Our Stars", Description ="Despite the tumor-shrinking medical miracle that has bought her a few years, Hazel has never been anything but terminal, her final chapter inscribed upon diagnosis. But when a gorgeous plot twist named Augustus Waters suddenly appears at Cancer Kid Support Group, Hazel's story is about to be completely rewritten. Insightful, bold, irreverent, and raw, `The Fault in Our Stars` is award-winning author John Green's most ambitious and heartbreaking work yet, brilliantly exploring the funny, thrilling, and tragic business of being alive and in love. This book comes from the `New York Times` bestselling author of `Looking for Alaska`, `An Abundance of Katherines`, `Paper Towns` and - with David Levithan - Will Grayson, Will Grayson. John Green has over 1.2 million Twitter followers, and almost 700,000 subscribers to Vlogbrothers, the YouTube channel he created with his brother, Hank. `The Fault in Our Stars` will capture a crossover audience in the same vein as Zadie Smith, David Nicholls' `One Day` and `Before I Die` by Jenny Downham. `Electric ...Filled with staccato bursts of humor and tragedy.` (`Jodi Picoult`)." , Price = 14, Rating = 10, ReleaseYear = 2014, Image = "https://images-na.ssl-images-amazon.com/images/I/51gnhI6hYdL._SX335_BO1,204,203,200_.jpg", AuthorId = 23 },
                 };
                 db.AddRange(initialBooks);
                 db.SaveChanges();
 
                var initialArtists = new List<Author>()
                 {
-                    new Author { Name = "Amy J Hackenber", DateOfBirth = "", Image = "", BookId = 25 }
+                    new Author { Name = "John Green", DateOfBirth = "24 August 1977", Image = "https://pmcvariety.files.wordpress.com/2015/07/john-green-first-look-deal-fox.jpg?w=700&h=394&crop=1", BookId = 29 }
                 };
                 db.AddRange(initialArtists);
                 db.SaveChanges(); 
