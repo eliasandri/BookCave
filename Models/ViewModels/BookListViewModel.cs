7e5611fd383646eb5032986ee0e8c5f8bbe0ed7b
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-05-06 19:00:57 +0000

    Omerkilegar breytingar
    omerkilegt

--- Models/ViewModels/BookListViewModel.cs
+++ Models/ViewModels/BookListViewModel.cs
@@ -6,13 +6,9 @@ namespace BookCave.Models.ViewModels
 
         public string Title { get; set; }
 
-        public int ReleaseYear { get; set; }
-
         public string Author { get; set; }
         
         public int AuthorId { get; set; }
-
-        public string Description { get; set; }
 
         public int Price { get; set; }
 
@@ -20,8 +16,5 @@ namespace BookCave.Models.ViewModels
 
         public string Image { get; set; }
 
-        public string Genre { get; set; }
-
-        public int GenreId { get; set; }
     }
 }
