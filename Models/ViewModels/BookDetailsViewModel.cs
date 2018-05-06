5893ee4b0d9a6a6347e7167ec02403b5af2d765d
Gudrun Heida <gudrunb16@ru.is>
2018-05-06 19:04:35 +0000

    byrjuð að vinna í bookdetails
    byrjuð að vinnaí bookdeitails

---
+++ Models/ViewModels/BookDetailsViewModel.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using BookCave.Data.EntityModels;
+namespace BookCave.Models.ViewModels
+
+{
+    public class BookDetailsListViewModel
+    {
+        public int BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public int Price { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Image { get; set; }
+        public string Description { get; set; }
+
+        public List<Author> authors { get; set; }
+        public int AuthorId { get; set; }
+        public string Genre { get; set; }
+        public int GenreId { get; set; }
+    }
+}
