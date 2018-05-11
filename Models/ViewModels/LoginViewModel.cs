7706bf5037bc63070eafe66ea6e2ea415a7fd118
ida16 <34029551+ida16@users.noreply.github.com>
2018-05-11 10:32:36 +0000

    LoginErrorHandling

--- Models/ViewModels/LoginViewModel.cs
+++ Models/ViewModels/LoginViewModel.cs
@@ -4,9 +4,10 @@ namespace BookCave.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
         [EmailAddress]
        public string Email { get; set; } 
-
+        [Required(ErrorMessage="Email or password incorrect. Please try again.")]
        public string Password { get; set; }
 
        public bool RememberMe { get; set; }
